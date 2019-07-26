0348d48cc8e79c9ad7f5ccb842fdc37b6a78672b
JanHaerensBeCode <haerens.jan@gmail.com>
2019-07-26 11:50:04 +0200

    get data from api and push in array

--- app.js
+++ app.js
@@ -6,12 +6,27 @@ const drawingBoard = d3.select('.drawBoard');
 //console.log(drawBoard);
 //console.log(drawingBoard);
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = 'https://canvasjs.com/services/data/datapoints.php)';
+
+fetch(proxyurl + url)
+        .then(response => response.json())
+        .then((contents) => {
+                console.log(contents);
+                for (i = 0 ; contents.length > i ; i++){
+                        convertedArray.push(contents[i][1]);
+                };
+                return convertedArray
+        });
+
+
+
 //add Data array                                                        (4)
 let randomData = [[0,6],[1,3],[2,1],[3,1],[4,1],[5,-4],[6,0],[7,-5],[8,-9],[9,-13]];
 let convertedArray = [];
-for (i = 0 ; randomData.length > i ; i++){
+/*for (i = 0 ; randomData.length > i ; i++){
         convertedArray.push(randomData[i][1]);
-};
+}; */
 console.log(convertedArray)
 
 //add an svg element                                                    (2)
@@ -105,9 +120,9 @@ dataNumbers.data(convertedArray)
         .attr('stroke', 'red')
         .attr('y', (d,i) => {
                 if (d >= 0){
-            return 290 - (d*10);
+            return 285 - (d*10);
                 } else {
-                        return 290
+                        return 285
                 }
     })
     .attr('x', (d, i) =>{
