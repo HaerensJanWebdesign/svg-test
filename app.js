57533ff3676394aedcce98d52d93eef8120d6280
JanHaerensBeCode <haerens.jan@gmail.com>
2019-08-20 13:29:52 +0200

    add comments for watch

--- app.js
+++ app.js
@@ -6,36 +6,27 @@ const drawingBoard = d3.select('.drawBoard');
 //console.log(drawBoard);
 //console.log(drawingBoard);
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = 'https://canvasjs.com/services/data/datapoints.php)';
-
-fetch(proxyurl + url)
-        .then(response => response.json())
-        .then((contents) => {
-                console.log(contents);
-                for (i = 0 ; contents.length > i ; i++){
-                        convertedArray.push(contents[i][1]);
-                };
-                return convertedArray
-        });
-
-
-
 //add Data array                                                        (4)
+
+/*
 let randomData = [[0,6],[1,3],[2,1],[3,1],[4,1],[5,-4],[6,0],[7,-5],[8,-9],[9,-13]];
 let convertedArray = [];
-/*for (i = 0 ; randomData.length > i ; i++){
+for (i = 0 ; randomData.length > i ; i++){
         convertedArray.push(randomData[i][1]);
-}; */
+}; 
 console.log(convertedArray)
+*/
 
-//add an svg element                                                    (2)
+//add an svg element 
+/*                                                   (2)
 const svg = drawingBoard.append('svg')
                         .attr('width', '800' )
                         .attr('height', '600');
+*/
 
 // draw some elements                                                   (3)
 
+/*
 svg.append('line')
 .attr('x1', 20)
 .attr('x2', 20)
@@ -70,6 +61,7 @@ svg.append('line')
         .attr('y1', 20)
         .attr('y2', 20)
         .attr('stroke', 'black');
+        */
 
 // // svg.append('text')
 //         .text('Epic SVG D3.js')
@@ -82,16 +74,23 @@ svg.append('line')
 
 // add rectangle to create data visualisation                          (5)
 
+/*
 for (i = 0; i < convertedArray.length; i++){
 svg.append('rect');
 svg.append('text');
 }
+*/
 
 //select all rectangles                                                 (6)
+
+/*
 const dataVisualization = d3.selectAll('rect');
 const dataNumbers = d3.selectAll('text');
+*/
 
 // give a position and size to each rectangle                           (7)
+
+/*
 dataVisualization.data(convertedArray)
         .attr('width', 63)
         .attr('fill','black')
@@ -131,3 +130,4 @@ dataNumbers.data(convertedArray)
     .attr('height', (d,i) => {
             return Math.abs(d * 10);
     });
+    */
